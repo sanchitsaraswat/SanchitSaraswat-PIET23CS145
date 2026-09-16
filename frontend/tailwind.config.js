bb904eb57b2a21484b46d55dbc06b7582a828dba
sanchitsaraswat <2023pietcssanchit145@poornima.org>
2026-09-16 11:01:29 +0000

    Use Poppins typography

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,2 +1,2 @@
 /** @type {import('tailwindcss').Config} */
-export default { content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: { colors: { ink: '#18212f', mint: '#147d64' } } }, plugins: [] };
+export default { content: ['./index.html', './src/**/*.{js,jsx}'], theme: { extend: { colors: { ink: '#18212f', mint: '#147d64' }, fontFamily: { sans: ['Poppins', 'ui-sans-serif', 'system-ui', 'sans-serif'] } } }, plugins: [] };
